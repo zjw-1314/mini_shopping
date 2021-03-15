41a494f0ba0ba8ed4a220455354d2c86046b22b1
zhangjiawei123 <2506641977@qq.com>
2021-03-15 23:33:44 +0800

    navigator finish

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,5 @@
+// 0 引入用来发送请求的方法，一定要把路径补全
+import { request } from "../../request/index.js";
 Page({
 
   /**
@@ -5,7 +7,9 @@ Page({
    */
   data: {
     // 轮播图数组
-    swiperList:[]
+    swiperList:[],
+    // 导航数组
+    cateList:[]
   },
 
   /**
@@ -13,63 +17,37 @@ Page({
    */
   // 页面开始加载就会触发
   onLoad: function (options) {
-    // 
+     // 1. 发送异步请求获取轮播图数据 优化的手段可以通过es6的promise来解决这个问题
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success:(result)=>{
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   }
+    // })
+     this.getSwiperList();
+     this.getCateList();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    // 1. 发送异步请求获取轮播图数据
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success:(result)=>{
-        this.setData({
-          swiperList:result.data.message
-        })
-      }
+  // 获取轮播图数据
+  getSwiperList(){
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
+    .then(res=>{
+      console.log(res)
+      this.setData({
+        swiperList:res.data.message
+      })
     })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  
+  // 获取分类导航数据
+  getCateList(){
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'})
+    .then(res=>{
+      this.setData({
+        cateList:res.data.message
+      })
+    })
   }
 })
